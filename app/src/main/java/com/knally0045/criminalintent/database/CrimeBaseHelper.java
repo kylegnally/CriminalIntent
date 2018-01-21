759488669b87d672f9b2fc5f4704680288f92ead
Kyle Nally <kylegnally@gmail.com>
2018-01-21 18:50:03 -0500

    Create database to store crimes into. Create CrimeBaseHelper. Create Crime DbSchema.

---
+++ app/src/main/java/com/knally0045/criminalintent/database/CrimeBaseHelper.java
@@ -0,0 +1,38 @@
+package com.knally0045.criminalintent.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Build;
+
+import com.knally0045.criminalintent.database.CrimeDbSchema.CrimeTable;
+
+/**
+ * Created by kyleg on 1/21/2018.
+ */
+
+public class CrimeBaseHelper extends SQLiteOpenHelper{
+
+    private static final int VERSION = 1;
+    public static final String DATABASE_NAME = "crimeBase.db";
+
+    public CrimeBaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + CrimeTable.NAME + "(" +
+                " _id integer primary key autoincrement, " +
+                CrimeTable.Cols.UUID + ", " +
+                CrimeTable.Cols.TITLE + ", " +
+                CrimeTable.Cols.DATE + ", " +
+                CrimeTable.Cols.SOLVED + ")"
+        );
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
---
+++ app/src/main/java/com/knally0045/criminalintent/database/CrimeDbSchema.java
@@ -0,0 +1,20 @@
+package com.knally0045.criminalintent.database;
+
+/**
+ * Created by kyleg on 1/21/2018.
+ */
+
+public class CrimeDbSchema {
+
+    public static final class CrimeTable {
+
+        public static final String NAME = "crimes";
+
+        public static final class Cols {
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String DATE = "date";
+            public static final String SOLVED = "solved";
+        }
+    }
+}
