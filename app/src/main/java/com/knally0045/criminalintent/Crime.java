68ad6c987b0bc34690c68ebad17b5ebeb8f2abc0
Kyle Nally <kylegnally@gmail.com>
2018-01-22 17:58:30 -0500

    Add implicit intent to start email app from selected contact.

--- app/src/main/java/com/knally0045/criminalintent/Crime.java
+++ app/src/main/java/com/knally0045/criminalintent/Crime.java
@@ -13,6 +13,7 @@ public class Crime {
     private String mTitle;
     private Date mDate;
     private boolean mSolved;
+    private String mSuspect;
 
 
     public Crime() {
@@ -23,6 +24,14 @@ public class Crime {
     public Crime(UUID id) {
         mId = id;
         mDate = new Date();
+    }
+
+    public String getSuspect() {
+        return mSuspect;
+    }
+
+    public void setSuspect(String suspect) {
+        mSuspect = suspect;
     }
 
     public Date getDate() {
